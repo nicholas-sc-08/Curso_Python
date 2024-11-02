5a03b7c606caf36b43c9479b9f8690e0138a729d
nicholas-sc-08 <nicholassc.2008@gmail.com>
2024-11-02 19:53:41 -0300

    Aprendendo estrutura condicional do if() em python

---
+++ Python_C5.py
@@ -0,0 +1,17 @@
+
+x = 20;
+y = 12;
+z = x * y;
+
+# estrutura condicional if;
+
+if z > x : print(z, "é maior que", x, sep="_");
+
+# estrutura condicional do else if;
+
+elif z == x: print(z,"é igual a", x, sep="_");
+
+# estrutura condicional do else;
+
+else : print(z, "é menor que", x, sep="_");
+
